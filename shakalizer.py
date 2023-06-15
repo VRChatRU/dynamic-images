c24bd73fa8c636a45db14d725044f3138daa8e53
Temmie Dola <temmiedola@gmail.com>
2023-06-15 15:16:20 +0300

    updated weekly image via .bat file

---
+++ shakalizer.py
@@ -0,0 +1,13 @@
+import os
+from PIL import Image
+
+dir_path = os.getcwd()
+
+for filename in os.listdir(dir_path):
+    if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
+        img = Image.open(filename)
+        new_size = (1024, 576)
+        img = img.resize(new_size)
+        new_filename = os.path.splitext(filename)[0] + '.webp'
+        img.save(new_filename, 'webp', lossless=False, quality=80)
+print('Готово!')
--- update-weekly.bat
+++ update-weekly.bat
@@ -1,4 +1,6 @@
-@echo off
-git add weekly.jpg
+python shakalizer.py
+git reset --soft HEAD~
+git add weekly.webp
 git commit -m "updated weekly image via .bat file"
-git push
+git push -f
+del /f weekly.png weekly.jpg weekly.jpeg
