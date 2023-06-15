b8a6aa03847585613c9f4c1796b75d0200c10214
TemmieDola <93981726+TemmieDola@users.noreply.github.com>
2023-06-15 15:18:25 +0300

    Update update-weekly.bat

--- update-weekly.bat
+++ update-weekly.bat
@@ -1,6 +1,6 @@
+@echo off
 python shakalizer.py
 git reset --soft HEAD~
 git add weekly.webp
 git commit -m "updated weekly image via .bat file"
 git push -f
-del /f weekly.png weekly.jpg weekly.jpeg
