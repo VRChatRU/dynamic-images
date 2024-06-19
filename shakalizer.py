3d3bc8ed6203e8519338e2c44302e85020971c95
Vard <vard@disroot.org>
2024-06-19 18:48:51 +0300

    weeklyHD fix4

--- shakalizer.py
+++ shakalizer.py
@@ -9,8 +9,8 @@ for filename in os.listdir(dir_path):
         hd_img = Image.open(filename)
         sd_img = sd_img.resize(1024, 576)
         sd_img.save(os.path.splitext('weekly')[0] + '.webp', 'webp', lossless=False, quality=80)
-        if hd_img.size.height[0] > 1920:
+        if hd_img.size[0] > 1920:
             hd_img = hd_img.resize(1920, 1080)
-        sd_img.save(os.path.splitext('weeklyHD')[0] + '.webp', 'webp', lossless=False, quality=100)
+        hd_img.save(os.path.splitext('weeklyHD')[0] + '.webp', 'webp', lossless=False, quality=100)
         os.remove(filename)
 print('Готово!')
